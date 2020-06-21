ca39e95eb77f42b2d9d3bc7df6ba40cfefc60466
Siddhartha Shukla <siddhartha.shukla.cer16@itbhu.ac.in>
2020-06-21 22:41:50 +0530

    redux setup completed

---
+++ src/redux/ActionCreator.js
@@ -0,0 +1,72 @@
+import * as ActionTypes from './ActionTypes';
+import MEDICINES from '../shared/medicines';
+import COMMENTS from '../shared/comments';
+
+export const fetchMedicine = () => (dispatch) => {
+
+    dispatch(medicinesLoading(true));
+
+    return fetch(MEDICINES)
+    .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
+    .then(response => response.json())
+    .then(medicines => dispatch(addMedicines(medicines)))
+    .catch(error => dispatch(medicinesFailed(error.message)));
+}
+
+export const medicinesLoading = () => ({
+    type: ActionTypes.MEDICINES_LOADING
+});
+
+export const medicinesFailed = (errmess) => ({
+    type: ActionTypes.MEDICINES_FAILED,
+    payload: errmess
+});
+
+export const addMedicines = (medicines) => ({
+    type: ActionTypes.ADD_MEDICINES,
+    payload: medicines
+});
+
+
+export const fetchComments = () => (dispatch) => {    
+    return fetch(COMMENTS)
+    .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
+    .then(response => response.json())
+    .then(comments => dispatch(addComments(comments)))
+    .catch(error => dispatch(commentsFailed(error.message)));
+};
+
+export const commentsFailed = (errmess) => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errmess
+});
+
+export const addComments = (comments) => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
